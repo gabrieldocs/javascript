2f941d868c363b74f30cab10a3f7202e5f6e0251
Lucas G. Santos <mydocs.lucas@gmail.com>
2022-03-24 17:43:47 -0300

    feat: inserting a sample JavaScript object to be used as a default

---
+++ defaults.js
@@ -0,0 +1,28 @@
+const objects = [
+    {
+        surname: "object_one",
+        mail: "object@one",
+        verbose_name: "Object One",
+        roles: [
+            "admin",
+            "manager",
+            "moderator",
+            "viewer",
+            "reviewer",
+            "user"
+        ],
+        age: 0
+    },
+    {
+        surname: "object_tow",
+        mail: "object@two",
+        verbose_name: "Object Two",
+        roles: [            
+            "manager",
+            "moderator",
+            "viewer",            
+            "user"
+        ],
+        age: 0
+    }
+]
